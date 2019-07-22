7253f09ccf0c4477ad74c7e5ebd3fcc6318ec998
Yerin Won <s2018w11@e-mirim.hs.kr>
2019-07-22 17:11:38 +0900

    JFrame, JPanel, JLabel

--- First/src/First.java
+++ First/src/First.java
@@ -1,9 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class First {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello World!");
+		JFrame frame = new JFrame("창이름");
+		
+		JPanel panel = new JPanel();
+		
+		JLabel label = new JLabel("기본값");
+		label.setText("잘 가");
+		
+		JLabel label2 = new JLabel();
+		label2.setText("가지마");
+		
+		frame.add(panel);
+		panel.add(label);
+		panel.add(label2);
+		panel.add(label2);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
